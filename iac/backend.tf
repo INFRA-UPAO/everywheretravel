42324de309e28fa2ce247319b15ef489f4f2058d
Gerson Portal <gportallinares21@gmail.com>
2026-06-17 09:20:00 -0500

    feat(iac): configure backend, providers and lock file

---
+++ iac/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket       = "everywhere-travel-tfstate"
+    key          = "terraform.tfstate"
+    region       = "us-east-2"
+    use_lockfile = true
+    encrypt      = true
+    profile      = "gpdev"
+  }
+}
