455dc5b4911b65b2edf15413f715a83ee3ef8dea
Gerson Portal <gportallinares21@gmail.com>
2026-06-18 10:10:00 -0500

    feat(module/kms): add CMK for ECR images

--- iac/modules/kms/main.tf
+++ iac/modules/kms/main.tf
@@ -314,3 +314,53 @@ resource "aws_kms_key_policy" "logs" {
     ]
   })
 }
+
+# CMK 7 — ECR
+
+resource "aws_kms_key" "ecr" {
+  description             = "Cifrado imágenes Docker en ECR - ${var.env}"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  multi_region            = false
+
+  tags = {
+    Name = "${var.prefix}-kms-ecr"
+  }
+}
+
+resource "aws_kms_alias" "ecr" {
+  name          = "alias/${var.prefix}/ecr"
+  target_key_id = aws_kms_key.ecr.key_id
+}
+
+resource "aws_kms_key_policy" "ecr" {
+  key_id = aws_kms_key.ecr.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableRootAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${local.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowECRService"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecr.amazonaws.com"
+        }
+        Action = [
+          "kms:CreateGrant",
+          "kms:DescribeKey",
+          "kms:GenerateDataKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
