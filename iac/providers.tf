e2df1949803ae40eecebd8152cde7ed06c99d289
Gerson Portal <gportallinares21@gmail.com>
2026-06-19 10:15:00 -0500

    fix(iac): remove hardcoded AWS profile from backend and providers

--- iac/providers.tf
+++ iac/providers.tf
@@ -17,7 +17,7 @@ provider "null" {
 
 provider "aws" {
   region  = var.aws_region
-  profile = "gpdev"
+
   alias   = "main"
 
   default_tags {
@@ -31,7 +31,7 @@ provider "aws" {
 
 provider "aws" {
   region  = "us-east-1"
-  profile = "gpdev"
+
   alias   = "edge"
 
   default_tags {
