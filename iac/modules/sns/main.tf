47eb9d126afd5f9e9d1bf4dd7c6f50a632cf2211
brunela-1 <valeriatbw@gmail.com>
2026-06-21 23:29:21 -0500

    feat(sns): add email subscription for alerts topic

--- iac/modules/sns/main.tf
+++ iac/modules/sns/main.tf
@@ -49,3 +49,10 @@ resource "aws_sns_topic_policy" "alerts" {
     ]
   })
 }
+
+resource "aws_sns_topic_subscription" "alerts_email" {
+  topic_arn                       = aws_sns_topic.alerts.arn
+  protocol                        = "email"
+  endpoint                        = var.alert_email
+  confirmation_timeout_in_minutes = 1
+}
