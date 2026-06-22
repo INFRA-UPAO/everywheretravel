0b884c817576795f7a94b14e7a718e99fbfeec73
brunela-1 <valeriatbw@gmail.com>
2026-06-21 23:19:16 -0500

    feat(sns): create alerts sns topic

--- iac/modules/sns/main.tf
+++ iac/modules/sns/main.tf
@@ -5,3 +5,12 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   region     = data.aws_region.current.region
 }
+
+resource "aws_sns_topic" "alerts" {
+  name = "${var.prefix}-alerts"
+
+  fifo_topic = false
+  tags = {
+    Name = "${var.prefix}-alerts"
+  }
+}
