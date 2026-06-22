6a6358c96a2cf212a8ed72e6016e49287cbbb5bd
brunela-1 <valeriatbw@gmail.com>
2026-06-21 23:46:08 -0500

    feat(sns): expose backup alerts outputs

--- iac/modules/sns/outputs.tf
+++ iac/modules/sns/outputs.tf
@@ -7,3 +7,13 @@ output "sns_alerts_name" {
   description = "Nombre del SNS Topic de alertas"
   value       = aws_sns_topic.alerts.name
 }
+
+output "sns_backup_arn" {
+  description = "ARN del SNS Topic de alertas de AWS Backup"
+  value       = aws_sns_topic.backup_alerts.arn
+}
+
+output "sns_backup_name" {
+  description = "Nombre del SNS Topic de backup alerts"
+  value       = aws_sns_topic.backup_alerts.name
+}
