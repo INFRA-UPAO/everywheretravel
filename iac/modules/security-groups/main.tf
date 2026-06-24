82dbff85841e752335da0ebeef6ff35d086e0998
brunela-1 <valeriatbw@gmail.com>
2026-06-24 16:27:29 -0500

    feat(security-groups): allow application access to interface endpoints

--- iac/modules/security-groups/main.tf
+++ iac/modules/security-groups/main.tf
@@ -226,3 +226,71 @@ resource "aws_vpc_security_group_egress_rule" "vpclink_to_alb" {
   ip_protocol                  = "tcp"
   description                  = "Trafico hacia ALB interno"
 }
+
+# SG VPC ENDPOINTS
+# SQS Endpoint
+resource "aws_vpc_security_group_ingress_rule" "vpce_sqs_from_ecs" {
+  security_group_id            = aws_security_group.vpce_sqs.id
+  referenced_security_group_id = aws_security_group.ecs_task.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde ECS Tasks"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpce_sqs_from_lambda" {
+  security_group_id            = aws_security_group.vpce_sqs.id
+  referenced_security_group_id = aws_security_group.lambda.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde Lambda doc-generante"
+}
+
+# Secrets Manager Endpoint
+resource "aws_vpc_security_group_ingress_rule" "vpce_sm_from_ecs" {
+  security_group_id            = aws_security_group.vpce_sm.id
+  referenced_security_group_id = aws_security_group.ecs_task.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde ECS Tasks"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpce_sm_from_lambda" {
+  security_group_id            = aws_security_group.vpce_sm.id
+  referenced_security_group_id = aws_security_group.lambda.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde Lambda doc-generante"
+}
+
+# CloudWatch Logs Endpoint
+resource "aws_vpc_security_group_ingress_rule" "vpce_logs_from_ecs" {
+  security_group_id            = aws_security_group.vpce_logs.id
+  referenced_security_group_id = aws_security_group.ecs_task.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde ECS Tasks"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "vpce_logs_from_lambda" {
+  security_group_id            = aws_security_group.vpce_logs.id
+  referenced_security_group_id = aws_security_group.lambda.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde Lambda doc-generante"
+}
+
+# ECR Endpoints
+resource "aws_vpc_security_group_ingress_rule" "vpce_ecr_from_ecs" {
+  security_group_id            = aws_security_group.vpce_ecr.id
+  referenced_security_group_id = aws_security_group.ecs_task.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS desde ECS Tasks para pull de imagenes"
+}
