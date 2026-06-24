d8cdea9be8b31e196c77f2ad9be7a0ae62291677
brunela-1 <valeriatbw@gmail.com>
2026-06-24 16:14:56 -0500

    feat(security-groups): configure lambda outbound connectivity

--- iac/modules/security-groups/main.tf
+++ iac/modules/security-groups/main.tf
@@ -167,3 +167,41 @@ resource "aws_vpc_security_group_ingress_rule" "rds_from_lambda" {
   ip_protocol                  = "tcp"
   description                  = "PostgreSQL desde Lambda doc-generante"
 }
+
+# SG LAMBDA
+# EGRESS
+resource "aws_vpc_security_group_egress_rule" "lambda_to_rds" {
+  security_group_id            = aws_security_group.lambda.id
+  referenced_security_group_id = aws_security_group.rds.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+  description                  = "PostgreSQL hacia RDS"
+}
+
+resource "aws_vpc_security_group_egress_rule" "lambda_to_vpce_sqs" {
+  security_group_id            = aws_security_group.lambda.id
+  referenced_security_group_id = aws_security_group.vpce_sqs.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoint SQS"
+}
+
+resource "aws_vpc_security_group_egress_rule" "lambda_to_vpce_sm" {
+  security_group_id            = aws_security_group.lambda.id
+  referenced_security_group_id = aws_security_group.vpce_sm.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoint Secrets Manager"
+}
+
+resource "aws_vpc_security_group_egress_rule" "lambda_to_vpce_logs" {
+  security_group_id            = aws_security_group.lambda.id
+  referenced_security_group_id = aws_security_group.vpce_logs.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoint CloudWatch Logs"
+}
