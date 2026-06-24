e90084aa2de2f0f463b69b7aad64694c917a11b6
brunela-1 <valeriatbw@gmail.com>
2026-06-24 15:08:13 -0500

    feat(security-groups): allow ecs access to vpc endpoints

--- iac/modules/security-groups/main.tf
+++ iac/modules/security-groups/main.tf
@@ -111,3 +111,40 @@ resource "aws_vpc_security_group_egress_rule" "ecs_to_rds" {
   ip_protocol                  = "tcp"
   description                  = "PostgreSQL hacia RDS"
 }
+
+# EGRESS
+resource "aws_vpc_security_group_egress_rule" "ecs_to_vpce_sqs" {
+  security_group_id            = aws_security_group.ecs_task.id
+  referenced_security_group_id = aws_security_group.vpce_sqs.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoint SQS"
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecs_to_vpce_sm" {
+  security_group_id            = aws_security_group.ecs_task.id
+  referenced_security_group_id = aws_security_group.vpce_sm.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoint Secrets Manager"
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecs_to_vpce_logs" {
+  security_group_id            = aws_security_group.ecs_task.id
+  referenced_security_group_id = aws_security_group.vpce_logs.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoint CloudWatch Logs"
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecs_to_vpce_ecr" {
+  security_group_id            = aws_security_group.ecs_task.id
+  referenced_security_group_id = aws_security_group.vpce_ecr.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
+  description                  = "HTTPS hacia VPC Endpoints ECR"
+}
