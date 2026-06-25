94a9f8451a75b8583a4a5e68b4c08834f978cecf
brunela-1 <valeriatbw@gmail.com>
2026-06-25 11:06:30 -0500

    feat(auth): configure user onboarding settings

--- iac/modules/auth/main.tf
+++ iac/modules/auth/main.tf
@@ -23,4 +23,21 @@ resource "aws_cognito_user_pool" "main" {
   username_configuration {
     case_sensitive = false
   }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+
+    invite_message_template {
+      email_subject = "Bienvenido a Everywhere Travel"
+      email_message = "Tu usuario es {username} y tu contraseña temporal es {####}"
+      sms_message   = "Tu usuario es {username} y tu contraseña es {####}"
+    }
+  }
 }
