721e6d96cbcd2ab1673f7b959f054fb9713e6e7b
brunela-1 <valeriatbw@gmail.com>
2026-06-25 16:22:04 -0500

    feat(api-gateway): create http api

--- iac/modules/api-gateway/main.tf
+++ iac/modules/api-gateway/main.tf
@@ -14,3 +14,33 @@ resource "aws_cloudwatch_log_group" "api_access_logs" {
     Name = "${var.prefix}-api-access-logs"
   }
 }
+
+# HTTP API v2
+resource "aws_apigatewayv2_api" "main" {
+  name          = "${var.prefix}-api"
+  protocol_type = "HTTP"
+  description   = "API HTTP v2 para Everywhere Travel - ${var.prefix}"
+
+  cors_configuration {
+    allow_origins = [
+      "https://${var.domain_name}",
+      "http://localhost:4200"
+    ]
+    allow_methods = [
+      "GET", "POST", "PUT",
+      "PATCH", "DELETE", "OPTIONS"
+    ]
+    allow_headers = [
+      "Authorization",
+      "Content-Type",
+      "X-Request-ID"
+    ]
+    expose_headers    = ["X-Request-ID"]
+    allow_credentials = true
+    max_age           = 3600
+  }
+
+  tags = {
+    Name = "${var.prefix}-api"
+  }
+}
